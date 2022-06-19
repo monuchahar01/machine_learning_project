36f0cf1863ebe548d56ac987e18294ea7461cc81
monu <monuchahar01@gmail.com>
2022-06-19 10:13:25 +0530

    new

--- test.py
+++ test.py
@@ -1,16 +1,5 @@
-from setuptools import setup,find_packages
 from typing import List
-
-
-#Declaring variables for setup functions
-PROJECT_NAME="housing-predictor"
-VERSION="0.0.2"
-AUTHOR="Avnish Yadav"
-DESRCIPTION="This is a first FSDS Nov batch Machine Learning Project"
-PACKAGES=["housing"]
 REQUIREMENT_FILE_NAME="requirements.txt"
-
-
 def get_requirements_list()->List[str]:
     """
     Description: This function is going to return list of requirement 
@@ -19,19 +8,12 @@ def get_requirements_list()->List[str]:
     of libraries mentioned in requirements.txt file
     """
     with open(REQUIREMENT_FILE_NAME) as requirement_file:
-        requirement_list =[lib_name.replace("\n","") for lib_name in  requirement_file.readlines()]
+        requirement_list = requirement_file.readlines()
         print(requirement_list)
         if "-e ." in requirement_list:
             requirement_list.remove("-e .")
         return requirement_list
 
 
-setup(
-name=PROJECT_NAME,
-version=VERSION,
-author=AUTHOR,
-description=DESRCIPTION,
-packages=find_packages(),
-install_requires=get_requirements_list()
-
-)
+if __name__=="__main__":
+    get_requirements_list()
